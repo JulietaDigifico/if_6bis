813ea826e93779158baafcfbe69f9b8974deb738
Julieta <julieta.digifico@gmail.com>
2024-02-12 05:59:22 -0300

    6bis ejercicio if

---
+++ app.py
@@ -0,0 +1,53 @@
+import tkinter
+from tkinter.messagebox import showinfo as alert
+from tkinter.messagebox import askyesno as question
+from tkinter.simpledialog import askstring as prompt
+import customtkinter
+
+'''
+nombre: Julieta
+apellido: Digifico
+---
+Ejercicio: if_06bis
+---
+Enunciado:
+
+A partir del ingreso de la altura en centímetros de un jugador de baloncesto, el programa deberá determinar la posición del jugador en la cancha, considerando los siguientes parametros:
+
+Menos de 160 cm: Base
+Entre 160 cm y 179 cm: Escolta
+Entre 180 cm y 199 cm: Alero
+200 cm o más: Ala-Pívot o Pívot
+
+'''
+
+class App(customtkinter.CTk):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("UTN FRA")
+
+        self.label1 = customtkinter.CTkLabel(master=self, text="Altura (cm)")
+        self.label1.grid(row=0, column=0, padx=20, pady=10)
+        
+        self.txt_altura = customtkinter.CTkEntry(master=self)
+        self.txt_altura.grid(row=0, column=1)
+                
+        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+        self.btn_mostrar.grid(row=2, pady=20, padx=30, columnspan=2, sticky="nsew")
+
+
+    def btn_mostrar_on_click(self):
+     altura_txt = self.txt_altura.get()
+     altura = int(altura_txt)
+     if altura < 160: Posición = "Base"
+     if 160 <= altura <= 179: Posición = "Escolta"
+     if 180 <= altura <= 199: Posición = "Alero"
+     if altura >= 200: Posición = "Pívot"
+     alert("Posición", f"La posición del jugador es {Posición}") 
+
+if __name__ == "__main__":
+    app = App()
+    app.geometry("300x300")
+    app.mainloop()
